5dec1c7d6c68cb844af53d045eec4269c27dadad
nilkud_the_black <asutsaian@gmail.com>
2024-12-08 08:03:29 +0300

    Update session.js

--- session.js
+++ session.js
@@ -52,5 +52,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=mJtFzSoJ#SqxcFArBmfq-GZx5wPiI2bvrmJyG7ZB9BumWT5R9lpU",
 };
